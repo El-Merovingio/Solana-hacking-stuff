3f8535db54cffd4a317e1c45a956ee56748d55fe
NonFungibleHacker <julioiglesiasp@gmail.com>
2022-09-14 12:43:21 -0400

    Add Level 2 native and fix

--- neodyme-workshop-remote-PoCs/level0-hack/native.sh
+++ neodyme-workshop-remote-PoCs/level0-hack/native.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cargo build-bpf --manifest-path=./Cargo.toml --bpf-out-dir=./target/so
 solana program deploy ./target/so/level0.so
-RUST_BACKTRACE=1 cargo run --bin native
+RUST_BACKTRACE=1 cargo run --manifest-path=./native/Cargo.toml --target-dir=./target/
--- neodyme-workshop-remote-PoCs/level1-hack/native.sh
+++ neodyme-workshop-remote-PoCs/level1-hack/native.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cargo build-bpf --manifest-path=./Cargo.toml --bpf-out-dir=./target/so
 solana program deploy ./target/so/level1.so
-RUST_BACKTRACE=1 cargo run --manifest-path=./pocs/Cargo.toml --target-dir=./target/
+RUST_BACKTRACE=1 cargo run --manifest-path=./native/Cargo.toml --target-dir=./target/
+#RUST_BACKTRACE=1 cargo run --bin native
